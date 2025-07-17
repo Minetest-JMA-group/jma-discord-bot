0118005cd94f8bd33dd42dc3ca80d2e50c9b1ea0
fancyfinn9 <fancyfinn9@proton.me>
2025-07-17 14:20:07 +0100

    PingReactCog: Add support multiple prompts, add debug command

--- cogs/pingreact.py
+++ cogs/pingreact.py
@@ -1,6 +1,14 @@
 import discord
 from discord.ext import commands
 
+phrases = {
+    "hello": "👋 Hello there!",
+    "relaylogin": "To login to the relays, DM the relay bot (**not the channel!**) `!login yourUsername yourPassword`",
+    "relay": "To chat in the relays, follow the instructions in https://discord.com/channels/1002108415511896095/1390797710638055505",
+    "pingsupport": "Please don't ping staff members unnecessarily!\nIf you require assistance, please open a ticket at https://discord.com/channels/1002108415511896095/1002110888859418725",
+    "support": "If you require assistance, please open a ticket at https://discord.com/channels/1002108415511896095/1002110888859418725",
+}
+
 class PingReactCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -19,12 +27,28 @@ class PingReactCog(commands.Cog):
         if self.bot.user in message.mentions:
             await message.add_reaction("👀")
 
-            if "hello" in message.content.lower():
-                target = message.reference.resolved if message.reference else message
-                await target.reply("👋 Hello there!")
+            for phrase in phrases:
+                if phrase in message.content.lower():
+                    target = message.reference.resolved if message.reference else message
+                    await target.reply(phrases[phrase])
+                    return
 
-        await self.bot.process_commands(message)
-            
+    @commands.hybrid_command()
+    async def list_ping_reactions(self, ctx):
+        """
+        Lists all the different ping reactions
+
+        Parameters
+        ----------
+        ctx: commands.Context
+            The context of the command invocation
+        """
+
+        prompts = ""
+        for phrase in phrases:
+            prompts = prompts + phrase + ", "
+        
+        await ctx.send(f"Here are all the prompts I'll respond to: {prompts}")
 
 async def setup(bot):
     await bot.add_cog(PingReactCog(bot))
