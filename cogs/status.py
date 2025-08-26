286e724dd4611be79d39bd9794646c94d7d6d1d8
SilentRipper_ <117438813+leoleg-SilentRipper@users.noreply.github.com>
2025-08-26 12:48:19 +0200

    Update status.py

--- cogs/status.py
+++ cogs/status.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-
+from discord import app_commands
 import os
 from dotenv import load_dotenv
 
@@ -18,9 +18,14 @@ class StatusCog(commands.Cog):
     async def cog_unload(bot):
         print("[StatusCog] Unloaded")
 
-    
-    @commands.hybrid_command()
-    async def set_status(self, ctx: commands.Context, activity_type: str, activity_name: str):
+    @app_commands.command()
+    @app_commands.describe(activity_name="Will be displayed as 'Playing activity_name'")
+    @app_commands.choices(activity_type=[
+        app_commands.Choice(name="Playing", value="playing"),
+        app_commands.Choice(name="Listening", value="listening"),
+        app_commands.Choice(name="Watching", value="watching")
+    ])
+    async def set_status(self, interaction: discord.Interaction, activity_type: app_commands.Choice[str], activity_name: str):
         """
         Sets the bot's status
 
@@ -34,21 +39,22 @@ class StatusCog(commands.Cog):
             Will be displayed as "Playing activity_name"
         """
 
-        has_role = any(role.id == role_botmanager for role in ctx.message.author.roles)
+        has_role = any(role.id == role_botmanager for role in interaction.user.roles)
         if not has_role:
-            await ctx.send("Hey, you don't have permissions to do that!", delete_after=5)
+            await interaction.response.send_message("Hey, you don't have permissions to do that!", ephemeral=True)
             return
 
-        if activity_type.lower() == "playing":
+        if activity_type.value.lower() == "playing":
             await self.bot.change_presence(activity=discord.Game(name=activity_name))
-        elif activity_type.lower() == "listening":
+        elif activity_type.value.lower() == "listening":
             await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=activity_name))
-        elif activity_type.lower() == "watching":
+        elif activity_type.value.lower() == "watching":
             await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=activity_name))
         else:
-            await ctx.send("Invalid activity type!")
+            await interaction.response.send_message("Invalid activity type!")
             return
-        await ctx.send(f"Status set to **{activity_type.lower()} {activity_name}**")
+
+        await interaction.response.send_message(f"Status set to **{activity_type.value.lower()} {activity_name}**")
 
 async def setup(bot):
     await bot.add_cog(StatusCog(bot))
